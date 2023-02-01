9f5e8d5ed1b128aeb6aef229dbf101f9aa2e536a
Elango <srielango@gmail.com>
2023-02-01 21:02:12 +0530

    Add project files.

---
+++ BlazorPoC/Pages/GoJsSample.razor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace BlazorPoC.Pages
+{
+    public partial class GoJsSample : ComponentBase
+    {
+        [Inject] IJSRuntime JSRuntime { get; set; }
+        protected async override void OnAfterRender(bool firstRender)
+        {
+            if (firstRender)
+            {
+                // This calls the script in gojs-scripts.js
+                await JSRuntime.InvokeAsync<string>("initGoJS");
+            }
+        }
+
+        protected async void Save()
+        {
+            await JSRuntime.InvokeAsync<string>("saveDiagram");
+        }
+
+        protected async void Load()
+        {
+            await JSRuntime.InvokeAsync<string>("loadDiagram");
+        }
+    }
+}
